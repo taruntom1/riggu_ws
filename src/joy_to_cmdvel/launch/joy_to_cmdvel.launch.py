f01c8cd7c32e53ef0bef687d4681d16cc534db1f
Tarun Tom <taruntom1@gmail.com>
2025-06-16 11:27:29 +0000

    commit 5

--- src/joy_to_cmdvel/launch/joy_to_cmdvel.launch.py
+++ src/joy_to_cmdvel/launch/joy_to_cmdvel.launch.py
@@ -4,12 +4,12 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         # Launch the joy node (joystick driver)
-        Node(
-            package='joy',
-            executable='joy_node',
-            name='joy_node',
-            output='screen'
-        ),
+        # Node(
+        #     package='joy',
+        #     executable='joy_node',
+        #     name='joy_node',
+        #     output='screen'
+        # ),
 
         # Launch your joystick-to-cmd_vel converter node
         Node(
@@ -19,7 +19,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'linear_axis': 1,       # Left stick vertical
-                'angular_axis': 0,      # Left stick horizontal
+                'angular_axis': 2,      # Left stick horizontal
                 'linear_scale': 1.0,
                 'angular_scale': 1.0
             }]
